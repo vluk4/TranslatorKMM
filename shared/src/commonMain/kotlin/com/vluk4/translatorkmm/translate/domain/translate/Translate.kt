243d580163b76df29226ba05124489192e547b16
Luca <victor_luca@mobills.com.br>
2023-01-06 01:15:40 -0300

    Use case from translating

---
+++ shared/src/commonMain/kotlin/com/vluk4/translatorkmm/translate/domain/translate/Translate.kt
@@ -0,0 +1,39 @@
+package com.vluk4.translatorkmm.translate.domain.translate
+
+import com.vluk4.translatorkmm.core.domain.language.Language
+import com.vluk4.translatorkmm.core.domain.util.Resource
+import com.vluk4.translatorkmm.translate.domain.translate.history.HistoryDataSource
+import com.vluk4.translatorkmm.translate.domain.translate.history.HistoryItem
+
+class Translate(
+    private val client: TranslateClient,
+    private val historyDataSource: HistoryDataSource
+) {
+
+    suspend fun execute(
+        fromLanguage: Language,
+        fromText: String,
+        toLanguage: Language
+    ): Resource<String> {
+        return try {
+            val translatedText = client.translate(
+                fromLanguage, fromText, toLanguage
+            )
+
+            historyDataSource.insertHistoryItem(
+                HistoryItem(
+                    id = null,
+                    fromLanguageCode = fromLanguage.langCode,
+                    fromText = fromText,
+                    toLanguageCode = toLanguage.langCode,
+                    toText = translatedText
+                )
+            )
+
+            Resource.Success(translatedText)
+        } catch (e: TranslateException) {
+            e.printStackTrace()
+            Resource.Error(e)
+        }
+    }
+}
